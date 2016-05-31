94a0622af9c380b868c781b7eb489a00634f96b7
Jan Jouke Tjalsma <j.j.tjalsma@library.leidenuniv.nl>
2016-05-31 13:40:34 +0200

    Updated formatting

--- javascript/main.js
+++ javascript/main.js
@@ -1,150 +1,163 @@
 $('#journalSrch').keypress(function(e) {
-                          if(e.which == 13) {
+    if (e.which == 13) {
         var searchTerm = $('#journalSrch').val();
         checkJournal(searchTerm);
         return false;
-                          }
-                          });
+    }
+});
 $('#findJournalBtn').click(function() {
-                           var searchTerm = $('#journalSrch').val();
-                           checkJournal(searchTerm);
-                           
+    var searchTerm = $('#journalSrch').val();
+    checkJournal(searchTerm);
+
 });
 
 function checkJournal(searchTerm) {
-              var url = "api/handler.php?action=search&q=" + searchTerm;
+    var url = "api/handler.php?action=search&q=" + searchTerm;
 
-    $.get(url, function( data ) {
-          $( "#searchResult" ).html( data.html );
-          });
+    $.get(url, function(data) {
+        $("#searchResult").html(data.html);
+    });
 }
 
 $('#journalSrch').autocomplete({
     serviceUrl: 'api/handler.php',
-    onSelect: function (suggestion) {
-                               $('#JournalIDSrch').val(suggestion.data);
-                               checkJournal(suggestion.value);
+    onSelect: function(suggestion) {
+        $('#JournalIDSrch').val(suggestion.data);
+        checkJournal(suggestion.value);
     }
 });
-$.get( "api/handler.php?action=stats", function( data ) {
-      $( "#overView" ).html( data.html );
-      });
+$.get("api/handler.php?action=stats", function(data) {
+    $("#overView").html(data.html);
+});
 
 
 
 
-      $('#journalTable').DataTable( {
+$('#journalTable').DataTable({
 
-                                   ajax: 'api/datatables.php',
-                                   processing: true,
-                                   serverSide: true,
-                                   dataSrc: 'data',
-                                   columns: [{data: 'Journal'}, {data: 'Reduction'}, {data: 'Publisher'}, {data:'Impact'},{data:'Quartile'},{data:'subject category', orderable: false}]
+    ajax: 'api/datatables.php',
+    processing: true,
+    serverSide: true,
+    dataSrc: 'data',
+    columns: [{
+        data: 'Journal'
+    }, {
+        data: 'Reduction'
+    }, {
+        data: 'Publisher'
+    }, {
+        data: 'Impact'
+    }, {
+        data: 'Quartile'
+    }, {
+        data: 'subject category',
+        orderable: false
+    }]
 
-                                   } );
+});
 
 $(document).ready(function() {
 
-                  // DataTable
-                  var table = $('#journalTable').DataTable();
+    // DataTable
+    var table = $('#journalTable').DataTable();
 
-                  table.on( 'xhr', function () {
-                           var json = table.ajax.json();
-
-
-                           var count = 0;
-                           $('#journalTable tfoot th').each( function () {
-                                                            var title = $(this).text();
-                                                            switch(count) {
-                                                            case (1):
-                                                            //discount
-                                                            var html = "<select>";
-
-                                                                     html = html + "<option></option>";
-                                                                     $.each(json.discountColumn, function(key,val){
-                                                                            // do something with key and val
-                                                                            if (json.discountColumn.length == 1) {
-                                                                            html = html + "<option selected>" + val + "</option>";
-                                                                            } else {
-                                                                            html = html + "<option>" + val + "</option>";
-                                                                            }
-                                                                            });
-
-                                                            html = html + "</select>";
-                                                            $(this).html(html);
-                                                            break;
-                                                            case (2):
-                                                            //publisher
-                                                            var html = "<select>";
-
-                                                            html = html + "<option></option>";
-                                                            $.each(json.publisherColumn, function(key,val){
-                                                                   // do something with key and val
-                                                                   if (json.publisherColumn.length == 1) {
-                                                                   html = html + "<option selected>" + val + "</option>";
-                                                                   } else {
-                                                                   html = html + "<option>" + val + "</option>";
-                                                                   }
-                                                                   });
-
-                                                            html = html + "</select>";
-                                                            $(this).html(html);
-                                                            break;
-                                                            case(4):
-                                                            //quartile
-                                                            var html = "<select>";
-
-                                                            html = html + "<option></option>";
-                                                            $.each(json.quartileColumn, function(key,val){
-                                                                   // do something with key and val
-                                                                   if (json.quartileColumn.length == 1) {
-                                                                   html = html + "<option selected>" + val + "</option>";
-                                                                   } else {
-                                                                   html = html + "<option>" + val + "</option>";
-                                                                   }
-                                                                   });
-
-                                                            html = html + "</select>";
-                                                            $(this).html(html);
-                                                            break;
-                                                            case(5):
-                                                            //categories
-                                                            var html = "<select>";
-
-                                                            html = html + "<option></option>";
-                                                            $.each(json.subjectColumn, function(key,val){
-                                                                   // do something with key and val
-                                                                   if (json.subjectColumn.length == 1) {
-                                                                   html = html + "<option selected>" + val + "</option>";
-                                                                   } else {
-                                                                   html = html + "<option>" + val + "</option>";
-                                                                   }
-                                                                   });
-
-                                                            html = html + "</select>";
-                                                            $(this).html(html);
-                                                            break;
-                                                            }
-
-                                                            count++;
-                                                            } );
+    table.on('xhr', function() {
+        var json = table.ajax.json();
 
 
+        var count = 0;
+        $('#journalTable tfoot th').each(function() {
+            var title = $(this).text();
+            switch (count) {
+                case (1):
+                    //discount
+                    var html = "<select>";
 
-                           // Apply the search
-                           table.columns().every( function () {
-                                                 var that = this;
+                    html = html + "<option></option>";
+                    $.each(json.discountColumn, function(key, val) {
+                        // do something with key and val
+                        if (json.discountColumn.length == 1) {
+                            html = html + "<option selected>" + val + "</option>";
+                        } else {
+                            html = html + "<option>" + val + "</option>";
+                        }
+                    });
 
-                                                 $( 'select', this.footer() ).on( 'change', function () {
-                                                                                if ( that.search() !== this.value ) {
-                                                                                that
-                                                                                .search( this.value )
-                                                                                .draw();
-                                                                                }
-                                                                                } );
-                                                 } );
+                    html = html + "</select>";
+                    $(this).html(html);
+                    break;
+                case (2):
+                    //publisher
+                    var html = "<select>";
 
-                           } );
+                    html = html + "<option></option>";
+                    $.each(json.publisherColumn, function(key, val) {
+                        // do something with key and val
+                        if (json.publisherColumn.length == 1) {
+                            html = html + "<option selected>" + val + "</option>";
+                        } else {
+                            html = html + "<option>" + val + "</option>";
+                        }
+                    });
+
+                    html = html + "</select>";
+                    $(this).html(html);
+                    break;
+                case (4):
+                    //quartile
+                    var html = "<select>";
+
+                    html = html + "<option></option>";
+                    $.each(json.quartileColumn, function(key, val) {
+                        // do something with key and val
+                        if (json.quartileColumn.length == 1) {
+                            html = html + "<option selected>" + val + "</option>";
+                        } else {
+                            html = html + "<option>" + val + "</option>";
+                        }
+                    });
+
+                    html = html + "</select>";
+                    $(this).html(html);
+                    break;
+                case (5):
+                    //categories
+                    var html = "<select>";
+
+                    html = html + "<option></option>";
+                    $.each(json.subjectColumn, function(key, val) {
+                        // do something with key and val
+                        if (json.subjectColumn.length == 1) {
+                            html = html + "<option selected>" + val + "</option>";
+                        } else {
+                            html = html + "<option>" + val + "</option>";
+                        }
+                    });
+
+                    html = html + "</select>";
+                    $(this).html(html);
+                    break;
+            }
+
+            count++;
+        });
 
 
-                             });
+
+        // Apply the search
+        table.columns().every(function() {
+            var that = this;
+
+            $('select', this.footer()).on('change', function() {
+                if (that.search() !== this.value) {
+                    that
+                        .search(this.value)
+                        .draw();
+                }
+            });
+        });
+
+    });
+
+
+});
